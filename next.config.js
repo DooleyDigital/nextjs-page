f0aad75349f6f0d0f93727310efce55b369d96db
Jordan Dooley <115346653+DooleyDigital@users.noreply.github.com>
2022-12-05 20:12:00 +0000

    loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {loader:"akamai",
+        path:"",
+  }
 }
 
 module.exports = nextConfig
